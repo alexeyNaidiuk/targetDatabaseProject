e2e10e3f4fd5ea9e40b0fab884b7fff6b5c7c4f0
alexey_naidiuk <alexeynaidiuk@gmail.com>
2022-12-01 13:03:23 +0200

    commit

--- tests/test_proxies_endpoint.py
+++ tests/test_proxies_endpoint.py
@@ -1,15 +1,64 @@
 import unittest
+from threading import Thread
 
 import requests
 
 from app.config import HOST, PORT
-from app.module.pools import WwmixProxyFilePool
+from app.module.pools import WwmixProxyFilePool, WestProxyFilePool, CheckedProxyFilePool
+
+
+def check_pool(proxy_pool, time_limit: int | None = None) -> list:
+    working = []
+    threads = []
+
+    def check_proxy(proxy):
+        proxies = {'http': proxy, 'https': proxy}
+        try:
+            response = requests.get('https://api.ipify.org', proxies=proxies, timeout=time_limit)
+            print(response.text)
+            if response:
+                working.append(f'http://{proxy}')
+        except Exception as e:
+            print(e)
+
+    for p in proxy_pool:
+        t = Thread(target=check_proxy, args=(p,))
+        t.start()
+        threads.append(t)
+    for t in threads:
+        t.join()
+
+    return working
 
 
 class TestProxiesEndpoint(unittest.TestCase):
 
-    def test_pool(self):
+    def test_wwmix_proxies(self):
         url = f'http://{HOST}:{PORT}/proxies/wwmix/pool'
-        response = requests.get(url)
-        value = response.content.decode()
-        self.assertListEqual(WwmixProxyFilePool().pool, value.split('\n'))
+        pool = requests.get(url).text.splitlines()
+        self.assertListEqual(WwmixProxyFilePool().pool, pool)
+
+        working = check_pool(pool, time_limit=10)
+        for p in working:
+            print(p)
+        self.assertNotEqual(working, [])
+
+    def test_west_proxies(self):
+        url = f'http://{HOST}:{PORT}/proxies/west/pool'
+        pool = requests.get(url).text.splitlines()
+        self.assertListEqual(WestProxyFilePool().pool, pool)
+
+        working = check_pool(pool, time_limit=10)
+        for p in working:
+            print(p)
+        self.assertNotEqual(working, [])
+
+    def test_checked_proxies(self):
+        url = f'http://{HOST}:{PORT}/proxies/checked/pool'
+        pool = requests.get(url).text.splitlines()
+        self.assertListEqual(CheckedProxyFilePool().pool, pool)
+
+        working = check_pool(pool, time_limit=10)
+        for p in working:
+            print(p)
+        self.assertNotEqual(working, [])
