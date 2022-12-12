dddbd70cd32a717b570d9333be45264cd920c82e
alexey_naidiuk <alexeynaidiuk@gmail.com>
2022-12-12 11:42:55 +0200

    commit

--- app/module/add_test_emails_to_database.py
+++ app/module/add_test_emails_to_database.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from app.config import TARGETS_FOLDER
+
 
 def get_database(path: Path | str):
     with open(path) as f:
@@ -12,18 +14,15 @@ def save_database(path: Path | str, list_to_save: list):
 
 
 def main():
-    original_list = get_database(Path(r'C:\Users\Administrator\Desktop\targetDatabaseProject\targets\dadru.csv'))
-
     suffix = 'dadru'
+    original_list = get_database(Path(TARGETS_FOLDER, f'{suffix}.csv'))
     step = 362
     c = 1
     while step <= len(original_list):
         original_list.insert(step, f'softumwork+{suffix}{c}@gmail.com')
         step += step
         c += 1
-
-    save_database(r'C:\Users\Administrator\Desktop\targetDatabaseProject\targets\test_dadru.csv', original_list)
-    print('softumwork+dadru1@gmail.com' in original_list)
+    save_database(Path(TARGETS_FOLDER, f'test_{suffix}.csv'), original_list)
 
 
 if __name__ == '__main__':
