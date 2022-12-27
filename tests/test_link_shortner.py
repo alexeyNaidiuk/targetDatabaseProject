606a42c80d68f21c0b53a1a682f2b0eb8f4f8de1
alexey_naidiuk <alexeynaidiuk@gmail.com>
2022-12-27 12:55:54 +0200

    bug fix

--- tests/test_link_shortner.py
+++ tests/test_link_shortner.py
@@ -19,9 +19,12 @@ class TestShortner(TestCase):
     def test_request_link(self):
         target_pool_name = 'mixru'
         referal_to_project = 'fortuneclock'
-
-        link = requests.get(
-            f'http://localhost:{PORT}/link',
-            params={'targets_base': target_pool_name, 'project_name': referal_to_project}
-        ).text
-        self.assertIn('bit.ly/', link)
+        response = None
+        while type(response) is not requests.Response:
+            try:
+                response = requests.get(f'http://localhost:{PORT}/link',
+                                   params={'targets_base': target_pool_name, 'project_name': referal_to_project})
+            except Exception as err:
+                logging.exception(err)
+            else:
+                self.assertIn('bit.ly/', response.text)
